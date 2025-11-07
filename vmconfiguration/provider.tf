bf3aa809a1739f91ec139c31a15cc24c29178680
jagadish88t <107781655+jagadish88t@users.noreply.github.com>
2025-11-06 21:18:17 -0500

    remove sub

--- vmconfiguration/provider.tf
+++ vmconfiguration/provider.tf
@@ -8,6 +8,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id                 = "28e1e42a-4438-4c30-9a5f-7d7b488fd883"
+  subscription_id                 = ""
   resource_provider_registrations = "none"
 }
